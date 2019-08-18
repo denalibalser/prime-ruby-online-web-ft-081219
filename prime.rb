a5624c0aad914e65de543eee2f9dbbacb7d55982
Denali Balser <denali.balser167@gmail.com>
2019-08-18 18:40:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
-def prime?(integer)
-  if integer % 2
-    false
+def prime?(number)
+  if number >= 2
+    (2..number - 1).all? do |x|
+      number % x != 0
+    end
   else
-    true
+    return false
   end
-end 
+end
